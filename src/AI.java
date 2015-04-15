3504cae79af7f82a912674003af49bbe7f24ab79
Eugene Karpenko <eugene.karpenko@filmon.com>
2015-04-15 18:41:13 +0300

    ai problem WIP

---
+++ src/AI.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+/**
+ * Created by Eugene Karpenko on 15.04.2015.
+ */
+public class AI {
+
+    private static final double EPSILON = 0.0001;
+
+    public static void main(String[] args) {
+        System.out.println("Загадай число. \n Сколько разрядов в твоём числе?");
+        Scanner scanner = new Scanner(System.in);
+        int digits = scanner.nextInt();
+        double minNumber = 0;
+        double maxNumber = Math.pow(10, digits);
+        double middle;
+        String answer;
+
+        while (true)
+        {
+            middle = Math.round((maxNumber + minNumber) / 2);
+            System.out.println("Твоё число больше " + middle + "? (y/n)");
+            answer = scanner.next();
+            if (answer.equals("y")) {
+                minNumber = middle + 1;
+            }
+            else if (answer.equals("n")) {
+                maxNumber = middle;
+            }
+
+            if (maxNumber - minNumber < EPSILON + 1) {
+                System.out.println("Твоё число " + minNumber + " :)");
+                return;
+            }
+        }
+    }
+}
